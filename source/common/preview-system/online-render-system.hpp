98f778aa9b85d348eaca6807cf692cd5c19766dc
Bassel Mostafa <bassel.mks97@gmail.com>
2022-04-07 17:43:51 +0200

    Updating ImGUI to use docking

--- source/common/preview-system/online-render-system.hpp
+++ source/common/preview-system/online-render-system.hpp
@@ -22,6 +22,10 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <imgui.h>
+#include <imgui_impl/imgui_impl_glfw.h>
+#include <imgui_impl/imgui_impl_opengl3.h>
+
 namespace racer
 {
     namespace previewSystem
@@ -47,7 +51,7 @@ namespace racer
                 glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
                 // Make window size fixed (User can't resize it)
-                glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+                glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
                 // Set Number of sample used in MSAA (0 = Disabled)
                 glfwWindowHint(GLFW_SAMPLES, 0);
@@ -120,6 +124,23 @@ namespace racer
 
                 gladLoadGL(glfwGetProcAddress); // Load the OpenGL functions from the driver
 
+                // Setup Dear ImGui context
+                IMGUI_CHECKVERSION();
+                ImGui::CreateContext();
+                ImGuiIO &io = ImGui::GetIO();
+                (void)io;
+                io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+                io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
+                io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable Multi-Viewport / Platform Windows
+
+                // Setup Dear ImGui style
+                ImGui::StyleColorsDark();
+                // ImGui::StyleColorsClassic();
+
+                // Setup Platform/Renderer backends
+                ImGui_ImplGlfw_InitForOpenGL(window, true);
+                ImGui_ImplOpenGL3_Init("#version 330");
+
                 setWindowIcon();
 
                 setPrimitiveModels();
@@ -130,17 +151,181 @@ namespace racer
                 glEnable(GL_DEPTH_TEST);
                 glDepthFunc(GL_LESS);
 
+                glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
                 Shader shader(MVP_VertexShader, diffuseFragmentShader);
+
+                // Our state
+                bool show_demo_window = true;
+                bool show_another_window = false;
+                ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
                 while (!glfwWindowShouldClose(window))
                 {
                     glfwPollEvents();
 
+                    // Start the Dear ImGui frame
+                    ImGui_ImplOpenGL3_NewFrame();
+                    ImGui_ImplGlfw_NewFrame();
+                    ImGui::NewFrame();
+
+                    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+                    /*if (show_demo_window)
+                        ImGui::ShowDemoWindow(&show_demo_window);
+
+                    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+                    {
+                        static float f = 0.0f;
+                        static int counter = 0;
+
+                        ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
+
+                        ImGui::Text("This is some useful text.");          // Display some text (you can use a format strings too)
+                        ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
+                        ImGui::Checkbox("Another Window", &show_another_window);
+
+                        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
+                        ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
+
+                        if (ImGui::Button("Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
+                            counter++;
+                        ImGui::SameLine();
+                        ImGui::Text("counter = %d", counter);
+
+                        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+                        ImGui::End();
+                    }
+
+                    // 3. Show another simple window.
+                    if (show_another_window)
+                    {
+                        ImGui::Begin("Another Window", &show_another_window); // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+                        ImGui::Text("Hello from another window!");
+                        if (ImGui::Button("Close Me"))
+                            show_another_window = false;
+                        ImGui::End();
+                    }*/
+
+                    static bool opt_fullscreen = true;
+                    static bool opt_padding = false;
+                    static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+
+                    // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+                    // because it would be confusing to have two docking targets within each others.
+                    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+                    if (opt_fullscreen)
+                    {
+                        const ImGuiViewport *viewport = ImGui::GetMainViewport();
+                        ImGui::SetNextWindowPos(viewport->WorkPos);
+                        ImGui::SetNextWindowSize(viewport->WorkSize);
+                        ImGui::SetNextWindowViewport(viewport->ID);
+                        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+                        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+                        window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+                        window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+                    }
+                    else
+                    {
+                        dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
+                    }
+
+                    // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
+                    // and handle the pass-thru hole, so we ask Begin() to not render a background.
+                    if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+                        window_flags |= ImGuiWindowFlags_NoBackground;
+
+                    // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+                    // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
+                    // all active windows docked into it will lose their parent and become undocked.
+                    // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
+                    // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+                    if (!opt_padding)
+                        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+                    ImGui::Begin("DockSpace Demo", NULL, window_flags);
+                    if (!opt_padding)
+                        ImGui::PopStyleVar();
+
+                    if (opt_fullscreen)
+                        ImGui::PopStyleVar(2);
+
+                    // Submit the DockSpace
+                    ImGuiIO &io = ImGui::GetIO();
+                    if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+                    {
+                        ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+                        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+                    }
+                    else
+                    {
+                        // ShowDockingDisabledMessage();
+                    }
+
+                    if (ImGui::BeginMenuBar())
+                    {
+                        if (ImGui::BeginMenu("Options"))
+                        {
+                            // Disabling fullscreen would allow the window to be moved to the front of other windows,
+                            // which we can't undo at the moment without finer window depth/z control.
+                            ImGui::MenuItem("Fullscreen", NULL, &opt_fullscreen);
+                            ImGui::MenuItem("Padding", NULL, &opt_padding);
+                            ImGui::Separator();
+
+                            if (ImGui::MenuItem("Flag: NoSplit", "", (dockspace_flags & ImGuiDockNodeFlags_NoSplit) != 0))
+                            {
+                                dockspace_flags ^= ImGuiDockNodeFlags_NoSplit;
+                            }
+                            if (ImGui::MenuItem("Flag: NoResize", "", (dockspace_flags & ImGuiDockNodeFlags_NoResize) != 0))
+                            {
+                                dockspace_flags ^= ImGuiDockNodeFlags_NoResize;
+                            }
+                            if (ImGui::MenuItem("Flag: NoDockingInCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_NoDockingInCentralNode) != 0))
+                            {
+                                dockspace_flags ^= ImGuiDockNodeFlags_NoDockingInCentralNode;
+                            }
+                            if (ImGui::MenuItem("Flag: AutoHideTabBar", "", (dockspace_flags & ImGuiDockNodeFlags_AutoHideTabBar) != 0))
+                            {
+                                dockspace_flags ^= ImGuiDockNodeFlags_AutoHideTabBar;
+                            }
+                            if (ImGui::MenuItem("Flag: PassthruCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode) != 0, opt_fullscreen))
+                            {
+                                dockspace_flags ^= ImGuiDockNodeFlags_PassthruCentralNode;
+                            }
+                            ImGui::Separator();
+
+                            /*if (ImGui::MenuItem("Close", NULL, false, p_open != NULL))
+                             *p_open = false;*/
+                            ImGui::EndMenu();
+                        }
+                        /*HelpMarker(
+                            "When docking is enabled, you can ALWAYS dock MOST window into another! Try it now!"
+                            "\n"
+                            "- Drag from window title bar or their tab to dock/undock."
+                            "\n"
+                            "- Drag from window menu button (upper-left button) to undock an entire node (all windows)."
+                            "\n"
+                            "- Hold SHIFT to disable docking (if io.ConfigDockingWithShift == false, default)"
+                            "\n"
+                            "- Hold SHIFT to enable docking (if io.ConfigDockingWithShift == true)"
+                            "\n"
+                            "This demo app has nothing to do with enabling docking!"
+                            "\n\n"
+                            "This demo app only demonstrate the use of ImGui::DockSpace() which allows you to manually create a docking node _within_ another window."
+                            "\n\n"
+                            "Read comments in ShowExampleAppDockSpace() for more details.");*/
+
+                        ImGui::EndMenuBar();
+                    }
+
+                    ImGui::ShowDemoWindow(&show_demo_window);
+                    ImGui::End();
+
+                    // Rendering
+                    ImGui::Render();
+
+                    glClearColor(
+                        activeScene->environmentColor.r, activeScene->environmentColor.g, activeScene->environmentColor.b,
+                        1.0f);
                     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-                    glClearColor(activeScene->environmentColor.r, activeScene->environmentColor.g, activeScene->environmentColor.b,
-                                 1.0f);
-
                     for (int i = 0; i < scene->shapesToRender.size(); i++)
                     {
                         shader.use();
@@ -166,9 +351,27 @@ namespace racer
                         sphereModel->Render();
                     }
 
+                    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+                    // Update and Render additional Platform Windows
+                    // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+                    //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
+                    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+                    {
+                        GLFWwindow *backup_current_context = glfwGetCurrentContext();
+                        ImGui::UpdatePlatformWindows();
+                        ImGui::RenderPlatformWindowsDefault();
+                        glfwMakeContextCurrent(backup_current_context);
+                    }
+
                     // Swap the frame buffers
                     glfwSwapBuffers(window);
                 }
+
+                // Cleanup
+                ImGui_ImplOpenGL3_Shutdown();
+                ImGui_ImplGlfw_Shutdown();
+                ImGui::DestroyContext();
 
                 // Destroy the window
                 glfwDestroyWindow(window);
